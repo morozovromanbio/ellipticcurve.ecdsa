16f7f7c7c0cc1494ef959a9bb02d0e66f16df261
Roman <n.e.w_w.a.v.e@mail.ru>
2021-12-29 23:42:03 +0300

    Create dip.py

---
+++ dip.py
@@ -0,0 +1,29 @@
+#Dependency inversion
+import sys
+import time
+
+
+class TerminalPrinter:
+    def write(self, msg):
+        sys.stderr.write(f"{msg}\n")
+
+
+class FilePrinter:
+    def write(self, msg):
+        with open('log.txt', 'a+', encoding='UTF8') as f:
+            f.write(f"{msg}\n")
+
+
+class Logger:
+    def __init__(self):
+        self.format = '%Y-%b-%d %H:%M:%S'
+
+
+    def log(self, message, notifier):
+        prefix = time.strftime(self.format, time.localtime())
+        notifier().write(f"{prefix} {message}")
+
+
+logger = Logger()
+logger.log("Starting the program...", TerminalPrinter)
+logger.log("An error!", FilePrinter)
